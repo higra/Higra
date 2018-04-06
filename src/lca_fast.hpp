c4e5e5165078695ccb4093280046bbcb8fc08421
Benjamin PERRET <benjamin.perret@esiee.fr>
2018-04-06 07:04:40 -0700

    Add LCA fast with n*log(n) preprocessing

---
+++ src/lca_fast.hpp
@@ -0,0 +1,130 @@
+//
+// Created by user on 4/6/18.
+//
+
+#pragma once
+
+#include "graph.hpp"
+
+namespace hg {
+    namespace lca_internal {
+        template<typename tree_t>
+        struct lca_fast {
+        private:
+
+            using array = xt::xtensor<std::size_t, 1>;
+            using array2d = xt::xtensor<std::size_t, 2>;
+            using vertex_t = typename tree_t::vertex_descriptor;
+
+            array Euler;
+            array Depth;
+            array Represent;
+            array Number;
+            array2d Minim;
+
+            std::size_t rep = 0;
+
+            std::size_t nbR;
+
+            long LCApreprocessDepthFirst(const tree_t &tree, std::size_t node, std::size_t depth, long *nbr,
+                                         std::size_t *rep) {
+                (*nbr)++;
+                Euler[*nbr] = node;
+                Number[node] = *nbr;
+                Depth[node] = depth;
+                Represent[*nbr] = node;
+                (*rep)++;
+                for (auto son: children_iterator(node, tree)) {
+                    LCApreprocessDepthFirst(tree, son, depth + 1, nbr, rep);
+                    Euler[++(*nbr)] = node;
+                }
+                return *nbr;
+            }
+
+
+            void LCApreprocess(const tree_t &tree) {
+                //O(n.log(n)) preprocessing
+                long nbr = -1;
+                std::size_t rep = 0;
+
+                nbr = LCApreprocessDepthFirst(tree, tree.root(), 0, &nbr, &rep);
+
+                // Check that the number of nodes in the tree was correct
+                std::size_t nbNodes = rep;
+                std::size_t nbRepresent = 2 * nbNodes - 1;
+                hg_assert((std::size_t) (nbr + 1) == nbRepresent, "oops incorrect number of nodes!");
+
+                std::size_t logn = (long) (ceil(log((double) (nbRepresent)) / log(2.0)));
+
+                Minim.resize({logn, nbRepresent});
+
+                for (std::size_t i = 0; i < nbRepresent - 1; i++) {
+                    if (Depth[Euler[i]] < Depth[Euler[i + 1]]) {
+                        Minim(0, i) = i;
+                    } else {
+                        Minim(0, i) = i + 1;
+                    }
+                }
+                Minim(0, nbRepresent - 1) = nbRepresent - 1;
+
+                for (std::size_t j = 1; j < logn; j++) {
+                    std::size_t k1 = 1 << (j - 1);
+                    std::size_t k2 = k1 << 1;
+                    for (std::size_t i = 0; i < nbRepresent; i++) {
+                        if ((i + k2) >= nbRepresent) {
+                            Minim(j, i) = nbRepresent - 1;
+                        } else {
+                            if (Depth[Euler[Minim(j - 1, i)]] <= Depth[Euler[Minim(j - 1, i + k1)]]) {
+                                Minim(j, i) = Minim(j - 1, i);
+                            } else {
+                                Minim(j, i) = Minim(j - 1, i + k1);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        public:
+            lca_fast(const tree_t &tree) {
+
+                auto nbNodes = num_vertices(tree);
+
+                Depth.resize({nbNodes});
+                Number.resize({nbNodes});
+
+                Euler.resize({2 * nbNodes - 1});
+                Represent.resize({2 * nbNodes - 1});
+
+                LCApreprocess(tree);
+            }
+
+            vertex_t lca(vertex_t n1, vertex_t n2) {
+                long ii, jj, kk, k;
+
+                ii = Number[n1];
+                jj = Number[n2];
+                if (ii == jj)
+                    return Represent[ii];
+
+                if (ii > jj) {
+                    kk = jj;
+                    jj = ii;
+                    ii = kk;
+                }
+
+                k = (long) (log((double) (jj - ii)) / log(2.));
+
+                if (Depth[Euler[Minim(k, ii)]] < Depth[Euler[Minim(k, jj - (1 << (k)))]]) {
+                    return Represent[Number[Euler[Minim(k, ii)]]];
+                } else {
+                    return Represent[Number[Euler[Minim(k, jj - (1 << k))]]];
+                }
+            }
+
+
+        };
+    }
+
+    using lca_fast = lca_internal::lca_fast<tree>;
+}
---
+++ test/test_lca.cpp
@@ -0,0 +1,46 @@
+//
+// Created by user on 3/22/18.
+//
+
+#include <boost/test/unit_test.hpp>
+#include "xtensor/xio.hpp"
+#include "graph.hpp"
+#include "lca_fast.hpp"
+#include "test_utils.hpp"
+
+BOOST_AUTO_TEST_SUITE(boost_lcafast);
+
+    using namespace hg;
+    using namespace std;
+
+
+    struct _data {
+
+        hg::tree t;
+
+        _data() : t(xt::xarray<long>{5, 5, 6, 6, 6, 7, 7, 7}) {
+        }
+
+    } data;
+
+
+    BOOST_AUTO_TEST_CASE(lca) {
+        auto t = data.t;
+        lca_fast lca(t);
+        BOOST_CHECK(lca.lca(0, 0) == 0);
+        BOOST_CHECK(lca.lca(3, 3) == 3);
+        BOOST_CHECK(lca.lca(5, 5) == 5);
+        BOOST_CHECK(lca.lca(7, 7) == 7);
+        BOOST_CHECK(lca.lca(0, 1) == 5);
+        BOOST_CHECK(lca.lca(1, 0) == 5);
+        BOOST_CHECK(lca.lca(2, 3) == 6);
+        BOOST_CHECK(lca.lca(2, 4) == 6);
+        BOOST_CHECK(lca.lca(3, 4) == 6);
+        BOOST_CHECK(lca.lca(5, 6) == 7);
+        BOOST_CHECK(lca.lca(0, 2) == 7);
+        BOOST_CHECK(lca.lca(1, 4) == 7);
+        BOOST_CHECK(lca.lca(2, 6) == 6);
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END();
