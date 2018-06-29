c161a06a1d93a0c650f996fa344bef62ba62030c
Benjamin Perret <benjamin.perret@esiee.fr>
2018-06-29 17:00:55 +0200

    Fibonacci heap progess

--- include/higra/structure/fibonacci_heap.hpp
+++ include/higra/structure/fibonacci_heap.hpp
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include <stack>
+#include <array>
 #include "../utils.hpp"
 
 namespace hg {
@@ -13,29 +14,52 @@ namespace hg {
     namespace fibonacci_heap_internal {
 
         template<typename T>
-        struct object_pool{
+        struct object_pool {
 
-            void free(const T * object){
-                auto i = object - &pool[0];
-                free_chain[i] = first_free;
-                first_free = i;
+
+            union object {
+                T t;
+                object *next;
+
+                object() {
+                    next = nullptr;
+                }
+
+                ~object() {
+
+                }
+            };
+
+
+            void free(const T *element) {
+                object *new_first = (object *) element;
+                new_first->next = first_free;
+                first_free = new_first;
             }
 
-            T* allocate(){
-                if(first_free != -1){
-                    index_t tmp = first_free;
-                    first_free = free_chain[first_free];
-                    return &pool[tmp];
+            T *allocate() {
+                if (first_free == nullptr) {
+                    pool.emplace_back(m_blocksize);
+                    auto &block = pool[pool.size() - 1];
+                    first_free = &block[0];
+                    for (index_t i = 0; i < block.size() - 1; i++)
+                        block[i].next = first_free + i + 1;
+                    block[block.size() - 1].next = nullptr;
                 }
-                pool.emplace_back();
-                free_chain.push_back(invalid_index);
-                return &pool[pool.size() - 1];
+
+                object *tmp = first_free;
+                first_free = first_free->next;
+                return (T *) tmp;
+            }
+
+            object_pool(size_t blocksize = 4096) : m_blocksize(blocksize) {
+
             }
 
         private:
-            std::vector<T> pool;
-            std::vector<T> free_chain;
-            index_t first_free = invalid_index;
+            std::vector<std::vector<object>> pool;
+            size_t m_blocksize;
+            object *first_free = nullptr;
         };
 
 
@@ -45,7 +69,7 @@ namespace hg {
         template<typename T>
         struct node {
             using self_t = node<T>;
-            using pointer_t = self_t*;
+            using pointer_t = self_t *;
         private:
             pointer_t m_previous;
             pointer_t m_next;
@@ -57,9 +81,9 @@ namespace hg {
             int m_degree;
             bool m_marked;
 
-            node(){}
+            node() {}
 
-            void init(T value){
+            void init(T value) {
                 m_value = value;
                 m_previous = this;
                 m_next = this;
@@ -72,7 +96,7 @@ namespace hg {
         public:
             friend class fibonacci_heap<T>;
 
-            T getValue() { return m_value; }
+            T get_value() { return m_value; }
         };
 
 
@@ -81,9 +105,14 @@ namespace hg {
             using node_t = node<T>;
 
         private:
-            static object_pool<node_t> s_pool;
-            node_t * m_heap = nullptr;
+            static object_pool<node_t> &s_pool() {
+                static object_pool<node_t> pool{};
+                return pool;
+            }
+
+            node_t *m_heap = nullptr;
             size_t m_size = 0;
+
         public:
             fibonacci_heap() {
 
@@ -97,65 +126,75 @@ namespace hg {
                 return m_heap == nullptr;
             }
 
-            auto & push(T value) {
+            auto &push(T value) {
                 m_size++;
-                node_t * new_node = s_pool.allocate();
+                node_t *new_node = s_pool().allocate();
                 new_node->init(value);
                 m_heap = m_merge(m_heap, new_node);
                 return *new_node;
             }
 
-            void merge(fibonacci_heap & other){
+            void merge(fibonacci_heap &other) {
                 m_heap = m_merge(m_heap, other.m_heap);
                 m_size += other.size();
                 other.m_size = 0;
                 other.m_heap = nullptr;
             }
 
-            auto & top(){
+            auto &top() {
                 return *m_heap;
             }
 
-            void pop(){
-                // TODO
+            void pop() {
+                m_extract_min();
             }
 
-            void clear(){
+            void update(node_t &node, const T &value) {
+
+            }
+
+            void clear() {
+                if (m_heap == nullptr)
+                    return;
+
                 std::stack<node_t *> s;
                 s.push(m_heap);
-                for(node_t * tmp = m_heap->next; tmp != m_heap; tmp = tmp->next){
+                for (node_t *tmp = m_heap->m_next; tmp != m_heap; tmp = tmp->m_next) {
                     s.push(tmp);
                 }
-                while(!s.empty()){
-                    node_t * n = s.top;
+                while (!s.empty()) {
+                    node_t *n = s.top();
                     s.pop();
-                    node_t * c = n->child;
-                    s.push(c);
-                    for(node_t * tmp = c->next; tmp != c; tmp = tmp->next){
-                        s.push(tmp);
+                    node_t *c = n->m_child;
+                    if (c != nullptr) {
+                        s.push(c);
+                        for (node_t *tmp = c->m_next; tmp != c; tmp = tmp->m_next) {
+                            s.push(tmp);
+                        }
                     }
-                    s_pool.free(n);
+                    s_pool().free(n);
                 }
                 m_heap = nullptr;
+                m_size = 0;
             }
 
-            auto size() const{
+            auto size() const {
                 return m_size;
             }
 
         private:
-            auto m_merge(node_t * root1, node_t * root2) {
-                if(root1 == nullptr)
+            auto m_merge(node_t *root1, node_t *root2) {
+                if (root1 == nullptr)
                     return root2;
-                if(root2 == nullptr)
+                if (root2 == nullptr)
                     return root1;
 
-                if(root1->value < root2->value){
+                if (root1->m_value < root2->m_value) {
                     std::swap(root1, root2);
                 }
 
-                node_t * root1_previous = root1->m_previous;
-                node_t * root2_next = root2->m_next;
+                node_t *root1_previous = root1->m_previous;
+                node_t *root2_next = root2->m_next;
 
                 root2->m_next = root1;
                 root1->m_previous = root2;
@@ -166,6 +205,97 @@ namespace hg {
                 return root2;
             }
 
+            void m_link_heap(node_t *y, node_t *x) {
+                y->m_previous->m_next = y->m_next;
+                y->m_next->m_previous = y->m_previous;
+
+                y->m_parent = x;
+                auto *child = x->m_child;
+                if(child != nullptr) {
+                    y->m_next = child->m_next;
+                    y->m_previous = child;
+                    child->m_next->m_previous = y;
+                    child->m_next = y;
+                }else{
+                    y->m_next = y;
+                    y->m_previous = y;
+                    x->m_child = y;
+                }
+                x->m_degree++;
+                y->m_marked = false;
+            }
+
+            void m_consolidate() {
+                if (m_heap == m_heap->m_next)
+                    return;
+
+                std::array<node_t *, 64> A;
+                std::fill(A.begin(), A.end(), nullptr);
+
+                auto *x = m_heap;
+                do {
+                    auto d = x->m_degree;
+                    while (A[d] != nullptr) {
+                        auto *y = A[d];
+                        if (y->m_value < x->m_value) {
+                            std::swap(x, y);
+                        }
+                        m_link_heap(y, x);
+                        A[d] = nullptr;
+                        d++;
+                    }
+                    A[d] = x;
+
+                    x = x->m_next;
+                } while (x != m_heap);
+
+                node_t *minh = nullptr;
+                int i = 0;
+                while (minh == nullptr) {
+                    if (A[i] != nullptr) {
+                        minh = A[i];
+                    }
+                    i++;
+                }
+
+                auto *start = minh;
+                auto *pos = minh->m_next;
+
+                while (pos != start) {
+                    if (pos->m_value < minh->m_value)
+                        minh = pos;
+                    pos = pos->m_next;
+                }
+                m_heap = minh;
+
+            }
+
+            void m_extract_min() {
+                if (m_heap != nullptr) {
+                    auto *child = m_heap->m_child;
+                    if(child != nullptr) {
+                        do {
+                            child->m_parent = nullptr;
+                            child = child->m_next;
+                        } while (child != m_heap->m_child);
+                    }
+
+                    m_merge(m_heap, m_heap->m_child);
+
+                    m_heap->m_previous->m_next = m_heap->m_next;
+                    m_heap->m_next->m_previous = m_heap->m_previous;
+
+                    auto *old_heap = m_heap;
+                    if (old_heap->m_next == old_heap) {
+                        m_heap = nullptr;
+                    } else {
+                        m_heap = old_heap->m_next;
+                        m_consolidate();
+                    }
+                    s_pool().free(old_heap);
+                    m_size--;
+                }
+            }
 
         };
     }
--- test/test_fibonacci_heap.cpp
+++ test/test_fibonacci_heap.cpp
@@ -6,20 +6,245 @@
 
 #include "higra/structure/fibonacci_heap.hpp"
 #include "test_utils.hpp"
+#include <random>
 
 BOOST_AUTO_TEST_SUITE(fibonacci_heap);
 
     using namespace hg;
     using namespace std;
 
+    template<typename T>
+    struct node {
 
-    BOOST_AUTO_TEST_CASE(testPool) {
-        fibonacci_heap_internal::object_pool<int> pool;
-        
+        auto get_value() {
+            return m_value;
+        }
+
+        node(T value) : m_value(value) {
+
+        }
+
+        T m_value;
+    };
+
+    template<typename T>
+    struct trivial_heap {
 
 
+        bool empty() const {
+            return elements.empty();
+        }
+
+        auto &push(T value) {
+            elements.emplace_back(value);
+            return elements[elements.size() - 1];
+        }
+
+        void merge(trivial_heap &other) {
+            for (auto n: other.elements)
+                elements.push_back(n);
+            other.elements.clear();
+        }
+
+        auto &top() {
+            return elements[imin()];
+        }
+
+        void pop() {
+            elements.erase(elements.begin() + imin());
+        }
+
+        void update(node<T> &node, const T &value) {
+            node.m_value = value;
+        }
+
+        void clear() {
+            elements.clear();
+        }
+
+        auto size() const {
+            return elements.size();
+        }
+
+    private:
+        vector<node<T>> elements;
+
+        index_t imin() {
+            T minv = elements[0].get_value();
+            index_t imini = 0;
+            for (index_t i = 1; i < elements.size(); i++) {
+                if (elements[i].get_value() < minv) {
+                    imini = i;
+                    minv = elements[i].get_value();
+                }
+            }
+            return imini;
+        }
+    };
+
+    BOOST_AUTO_TEST_CASE(test_pool_one_block) {
+        fibonacci_heap_internal::object_pool<long> pool;
+        long *i1 = pool.allocate();
+
+        long *i2 = pool.allocate();
+        BOOST_CHECK(i2 - i1 == 1);
+        long *i3 = pool.allocate();
+        BOOST_CHECK(i3 - i1 == 2);
+        long *i4 = pool.allocate();
+        BOOST_CHECK(i4 - i1 == 3);
+
+        pool.free(i3);
+
+        long *i5 = pool.allocate();
+        BOOST_CHECK(i5 - i1 == 2);
+        long *i6 = pool.allocate();
+        BOOST_CHECK(i6 - i1 == 4);
+
+        pool.free(i5);
+        pool.free(i4);
+
+        long *i7 = pool.allocate();
+        BOOST_CHECK(i7 - i1 == 3);
+        long *i8 = pool.allocate();
+        BOOST_CHECK(i8 - i1 == 2);
+        long *i9 = pool.allocate();
+        BOOST_CHECK(i9 - i1 == 5);
+        long *i10 = pool.allocate();
+        BOOST_CHECK(i10 - i1 == 6);
+    }
+
+    BOOST_AUTO_TEST_CASE(test_pool_several_blocks) {
+        fibonacci_heap_internal::object_pool<long> pool(3);
+        long *i1 = pool.allocate();
+        long *i2 = pool.allocate();
+        BOOST_CHECK(i2 - i1 == 1);
+        long *i3 = pool.allocate();
+        BOOST_CHECK(i3 - i1 == 2);
+
+        long *i4 = pool.allocate();
+        long *i5 = pool.allocate();
+        BOOST_CHECK(i5 - i4 == 1);
+        long *i6 = pool.allocate();
+        BOOST_CHECK(i6 - i4 == 2);
+
+        long *i7 = pool.allocate();
+        long *i8 = pool.allocate();
+        BOOST_CHECK(i8 - i7 == 1);
+
+        pool.free(i6);
+        pool.free(i2);
+        pool.free(i4);
+
+        long *i9 = pool.allocate();
+        BOOST_CHECK(i9 - i4 == 0);
+        long *i10 = pool.allocate();
+        BOOST_CHECK(i10 - i1 == 1);
+        long *i11 = pool.allocate();
+        BOOST_CHECK(i11 - i4 == 2);
+
+        long *i12 = pool.allocate();
+        BOOST_CHECK(i12 - i7 == 2);
+
+        long *i13 = pool.allocate();
+        long *i14 = pool.allocate();
+        BOOST_CHECK(i14 - i13 == 1);
+    }
+
+    BOOST_AUTO_TEST_CASE(test_fibonacci_heap1) {
+        fibonacci_heap_internal::fibonacci_heap<long> heap;
+        heap.push(10);
+        BOOST_CHECK(heap.size() == 1);
+        BOOST_CHECK(!heap.empty());
+        BOOST_CHECK(heap.top().get_value() == 10);
+        heap.push(15);
+        BOOST_CHECK(heap.size() == 2);
+        BOOST_CHECK(heap.top().get_value() == 10);
+        heap.push(8);
+        BOOST_CHECK(heap.size() == 3);
+        BOOST_CHECK(heap.top().get_value() == 8);
+
+        heap.clear();
+        BOOST_CHECK(heap.size() == 0);
+        BOOST_CHECK(heap.empty());
+    }
+
+    BOOST_AUTO_TEST_CASE(test_fibonacci_heap2) {
+        fibonacci_heap_internal::fibonacci_heap<long> heap;
+        heap.push(10);
+        heap.pop();
+        BOOST_CHECK(heap.size() == 0);
+        BOOST_CHECK(heap.empty());
+
+        heap.push(10);
+        heap.push(15);
+        heap.push(8);
+
+        BOOST_CHECK(heap.top().get_value() == 8);
+        heap.pop();
+        BOOST_CHECK(heap.size() == 2);
+        BOOST_CHECK(heap.top().get_value() == 10);
+        heap.pop();
+        BOOST_CHECK(heap.size() == 1);
+        BOOST_CHECK(heap.top().get_value() == 15);
+        heap.pop();
+        BOOST_CHECK(heap.size() == 0);
     }
 
 
+    BOOST_AUTO_TEST_CASE(test_fibonacci_heap3) {
+        fibonacci_heap_internal::fibonacci_heap<long> heap;
+        heap.push(10);
+        heap.push(15);
+        heap.push(8);
+
+        fibonacci_heap_internal::fibonacci_heap<long> heap2;
+        heap.push(9);
+        heap.push(7);
+
+        heap.merge(heap2);
+        BOOST_CHECK(heap.top().get_value() == 7);
+        heap.pop();
+        BOOST_CHECK(heap.top().get_value() == 8);
+        heap.pop();
+        BOOST_CHECK(heap.top().get_value() == 9);
+        heap.pop();
+        BOOST_CHECK(heap.top().get_value() == 10);
+        heap.pop();
+        BOOST_CHECK(heap.top().get_value() == 15);
+        heap.pop();
+        BOOST_CHECK(heap.size() == 0);
+
+    }
+
+    BOOST_AUTO_TEST_CASE(test_fibonacci_heap_stress_test_push_pop) {
+        std::mt19937 rng;
+        rng.seed(std::random_device()());
+        std::uniform_int_distribution<std::mt19937::result_type> dist100(1, 100); // distribution in range [1, 6]
+
+        std::uniform_int_distribution<std::mt19937::result_type> weights(1, 100000); // distribution in range [1, 6]
+
+        fibonacci_heap_internal::fibonacci_heap<long> heap;
+        trivial_heap<long> theap;
+
+        int nbop = 1000;
+
+        for (int i = 0; i < nbop; i++) {
+            int op = dist100(rng);
+            if (op < 80) {
+                auto w = weights(rng);
+                heap.push(w);
+                theap.push(w);
+            } else {
+                BOOST_CHECK(heap.top().get_value() == theap.top().get_value());
+                BOOST_CHECK(heap.size() == theap.size());
+                heap.pop();
+                theap.pop();
+                BOOST_CHECK(heap.top().get_value() == theap.top().get_value());
+                BOOST_CHECK(heap.size() == theap.size());
+            }
+
+        }
+
+    }
 
 BOOST_AUTO_TEST_SUITE_END();
