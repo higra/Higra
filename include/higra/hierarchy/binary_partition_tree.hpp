8b533f243e40ebcc407ffb68a240b9167374a6c5
Benjamin Perret <benjamin.perret@esiee.fr>
2018-07-17 13:49:24 +0200

    Start of binary partition tree

---
+++ include/higra/hierarchy/binary_partition_tree.hpp
@@ -0,0 +1,16 @@
+//
+// Created by perretb on 17/07/18.
+//
+
+#pragma once
+
+#include "../graph.hpp"
+#include "../structure/fibonacci_heap.hpp"
+
+namespace hg {
+
+    namespace binary_partition_tree_internal {
+
+
+    }
+}
---
+++ test/test_binary_partition_tree.cpp
@@ -0,0 +1,18 @@
+#include <boost/test/unit_test.hpp>
+#include "test_utils.hpp"
+#include "higra/hierarchy/binary_partition_tree.hpp"
+//
+// Created by user on 3/9/18.
+//
+
+
+
+
+BOOST_AUTO_TEST_SUITE(test_binary_partition_tree);
+
+    BOOST_AUTO_TEST_CASE(test_single_linkage_clustering) {
+
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END();
