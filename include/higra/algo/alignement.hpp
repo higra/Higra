3738adb9d2ac8e45f6e7e7bd516e00d67680bace
Benjamin Perret <benjamin.perret@esiee.fr>
2018-10-26 14:24:01 +0200

    Hierarchy alignement (cpp)

--- include/higra/algo/alignement.hpp
+++ include/higra/algo/alignement.hpp
@@ -10,9 +10,9 @@
 
 #pragma once
 
-#include "../graph.hpp"
 #include "rag.hpp"
-#include "higra/structure/lca_fast.hpp"
+#include "tree.hpp"
+#include "higra/hierarchy/hierarchy_core.hpp"
 #include "xtensor/xsort.hpp"
 
 namespace hg {
@@ -53,15 +53,15 @@ namespace hg {
         hg_assert(labelisation_fine.size() == labelisation_coarse.size(),
                   "Labelisations must have the same size.");
 
-        if(num_regions_fine == 0){
+        if (num_regions_fine == 0) {
             num_regions_fine = xt::amax(labelisation_fine)(0) + 1;
         }
 
-        if(num_regions_coarse == 0){
+        if (num_regions_coarse == 0) {
             num_regions_coarse = xt::amax(labelisation_coarse)(0) + 1;
         }
 
-        array_2d <size_t> intersections = xt::zeros<size_t>({num_regions_fine, num_regions_coarse});
+        array_2d<size_t> intersections = xt::zeros<size_t>({num_regions_fine, num_regions_coarse});
 
         for (index_t i = 0; i < labelisation_fine.size(); i++) {
             intersections(labelisation_fine(i), labelisation_coarse(i))++;
@@ -89,18 +89,20 @@ namespace hg {
                                                    num_vertices(coarse_rag.rag));
     }
 
-    namespace alignement_internal{
+    namespace alignement_internal {
 
-        template <typename rag_t, typename T, typename tree_t, typename T2>
-        auto project_hierarchy(const rag_t & rag_fine, const T & coarse_supervertices, const tree_t & tree_coarse, const T2 & tree_coarse_node_altitudes){
-            auto & fine_supervertices = rag_fine.vertex_map;
-            auto & rag = rag_fine.rag;
+        template<typename rag_t, typename T, typename tree_t, typename T2>
+        auto project_hierarchy(const rag_t &rag_fine, const T &coarse_supervertices, const tree_t &tree_coarse,
+                               const T2 &tree_coarse_node_altitudes) {
+            auto &fine_supervertices = rag_fine.vertex_map;
+            auto &rag = rag_fine.rag;
 
             auto fine_to_coarse_map = project_fine_to_coarse_labelisation(fine_supervertices, coarse_supervertices);
-            array_1d<typename T2::value_type> coarse_sm_on_fine_rag = xt::empty({num_edges(rag_fine.rag)});
+            array_1d<typename T2::value_type> coarse_sm_on_fine_rag = xt::empty<typename T2::value_type>(
+                    {num_edges(rag_fine.rag)});
 
             lca_fast lca(tree_coarse);
-            for(auto e: edge_iterator(rag)){
+            for (auto e: edge_iterator(rag)) {
                 auto projected_lca = lca.lca(fine_to_coarse_map[source(e, rag)], fine_to_coarse_map[target(e, rag)]);
                 coarse_sm_on_fine_rag(index(e, rag)) = tree_coarse_node_altitudes(projected_lca);
             }
@@ -108,24 +110,93 @@ namespace hg {
             return coarse_sm_on_fine_rag;
         }
     }
-/*
 
-    class hierarchy_aligner{
-                public:
+    /**
+     * This class allows to project hierarchies build from coarse supervertices
+     * onto fine supervertices.
+     *
+     * The class is contructed by providing a fine supervertices decomposition of a graph.
+     * Then the functions align_hierarchy allows to project a hierarchy, given as a tree or as a saliency map,
+     * onto the fine supervertices.
+     *
+     * Given:
+     *  - a graph g
+     *  - a fine labelisation l1 of the vertices of g;
+     *  - a tree t on g whose supervertices corresponds to the coarse labelisation l2 of the vertices of g; and
+     *  - the altitudes a of the nodes of t.
+     * Let us denote:
+     *  - given a vertex x of g and a labelisation l, l(x) is the region of l that contains x
+     *  - given a region r of l1, s(r, l2) is the region R of l2 that has the largest intersection with r:
+     *            s(r, l2) = arg_max(R in l2) |R \cap r|
+     * The projection of t onto l1 is a hierarchy given by the saliency map sm on g defined by:
+     *     for all {x,y} in edges(g), sm({x,y}) = a(lca_t(s(l1(x), l2), s(l1(y), l2)))
+     *
+     * See the following helper functions for instanciation
+     *  - make_hierarchy_aligner_from_graph_cut
+     *  - make_hierarchy_aligner_from_labelisation
+     *  - make_hierarchy_aligner_from_hierarchy
+     */
+    class hierarchy_aligner {
+    public:
 
-                    template<typename graph_t, typename T>
-                    hierarchy_aligner(const graph_t & graph, const xt::xexpression<T> & saliency_map) :
-                    m_fine_rag(make_region_adjacency_graph(graph, saliency_map)){
-                    }
+        hierarchy_aligner(const region_adjacency_graph &rag) : m_fine_rag(rag) {
 
-                    template<typename graph_t, typename tree_t>
-                    hierarchy_aligner(const graph_t & graph, const tree_t & tree) :
-                    fine_rag()
+        }
 
-                private:
-                    region_adjacency_graph m_fine_rag;
-                };
-*/
+        template<typename T>
+        auto align_hierarchy(const hg::tree &tree, const xt::xexpression<T> &xaltitudes) const {
+            auto &altitudes = xaltitudes.derived_cast();
+            hg_assert_node_weights(tree, altitudes);
+            hg_assert_1d_array(altitudes);
+            hg_assert(num_leaves(tree) == m_fine_rag.vertex_map.size(),
+                      "Cannot align given hierarchy: incompatible sizes!");
+            auto sv_hierarchy = supervertices_hierarchy(tree, altitudes);
+            auto altitudes_sv_hierarchy = xt::index_view(altitudes, sv_hierarchy.node_map);
+            auto coarse_sm_on_fine_rag =
+                    alignement_internal::project_hierarchy(m_fine_rag,
+                                                           sv_hierarchy.supervertex_labelisation,
+                                                           sv_hierarchy.tree,
+                                                           altitudes_sv_hierarchy);
+            return rag_back_project_weights(m_fine_rag.edge_map, coarse_sm_on_fine_rag);
+        }
 
+        template<typename graph_t, typename T>
+        auto align_hierarchy(const graph_t &graph, const xt::xexpression<T> &xsaliency_map) const {
+            auto &saliency_map = xsaliency_map.derived_cast();
+            hg_assert_edge_weights(graph, saliency_map);
+            hg_assert_1d_array(saliency_map);
+            hg_assert(num_vertices(graph) == m_fine_rag.vertex_map.size(),
+                      "Cannot align given hierarchy: incompatible sizes!");
+            auto coarse_rag = make_region_adjacency_graph_from_graph_cut(graph, saliency_map);
+            auto coarse_rag_edge_weights = rag_accumulate(coarse_rag.edge_map, saliency_map, accumulator_first());
+            auto bpt_coarse_rag = bpt_canonical(coarse_rag.rag, coarse_rag_edge_weights);
+            auto coarse_sm_on_fine_rag =
+                    alignement_internal::project_hierarchy(m_fine_rag,
+                                                           coarse_rag.vertex_map,
+                                                           bpt_coarse_rag.tree,
+                                                           bpt_coarse_rag.altitudes);
+            return rag_back_project_weights(m_fine_rag.edge_map, coarse_sm_on_fine_rag);
+        }
+
+    private:
+        region_adjacency_graph m_fine_rag;
+    };
+
+    template<typename graph_t, typename T>
+    auto make_hierarchy_aligner_from_graph_cut(const graph_t &graph, const xt::xexpression<T> &saliency_map) {
+        return hierarchy_aligner(make_region_adjacency_graph_from_graph_cut(graph, saliency_map));
+    }
+
+    template<typename graph_t, typename T>
+    auto make_hierarchy_aligner_from_labelisation(const graph_t &graph, const xt::xexpression<T> &vertex_labels) {
+        return hierarchy_aligner(make_region_adjacency_graph_from_labelisation(graph, vertex_labels));
+    }
+
+    template<typename graph_t, typename tree_t, typename T>
+    auto make_hierarchy_aligner_from_hierarchy(const graph_t &graph, const tree_t &tree,
+                                               const xt::xexpression<T> &altitudes) {
+        m_fine_rag(make_region_adjacency_graph_from_labelisation(graph, labelisation_hierarchy_supervertices(tree,
+                                                                                                             altitudes)));
+    }
 
 }
--- test/test_alignement.cpp
+++ test/test_alignement.cpp
@@ -10,6 +10,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include "higra/algo/alignement.hpp"
+#include "higra/image/graph_image.hpp"
 #include "test_utils.hpp"
 
 
@@ -26,6 +27,58 @@ BOOST_AUTO_TEST_SUITE(alignement_test);
 
         array_1d<int> ref_map{0, 1, 2, 0, 2};
         BOOST_CHECK(ref_map == map);
+    }
+
+    BOOST_AUTO_TEST_CASE(test_hierarchy_alignement) {
+
+        auto g = get_4_adjacency_graph({3, 3});
+        array_1d<int> fine_labels{0, 1, 2, 3, 4, 2, 3, 4, 2};
+
+        auto aligner = make_hierarchy_aligner_from_labelisation(g, fine_labels);
+
+        hg::tree t(array_1d<index_t>{9, 10, 10, 9, 11, 11, 9, 11, 11, 13, 12, 12, 13, 13});
+        array_1d<int> altitudes{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2};
+        auto sm = aligner.align_hierarchy(t, altitudes);
+        auto sm_k = graph_4_adjacency_2_khalimsky(g, {3, 3}, sm);
+
+        array_1d<int> sm_coarse{2, 0, 0, 1, 1, 2, 0, 0, 0, 0, 2, 0};
+        auto sm2 = aligner.align_hierarchy(g, sm_coarse);
+        auto sm2_k = graph_4_adjacency_2_khalimsky(g, {3, 3}, sm2);
+
+        array_2d<int> sm_k_ref = {{0, 2, 0, 1, 0},
+                                  {0, 2, 1, 1, 0},
+                                  {0, 2, 0, 0, 0},
+                                  {0, 2, 0, 0, 0},
+                                  {0, 2, 0, 0, 0}};
+
+        BOOST_CHECK(sm_k == sm_k_ref);
+        BOOST_CHECK(sm2_k == sm_k_ref);
+    }
+
+    BOOST_AUTO_TEST_CASE(test_hierarchy_alignement2) {
+
+        auto g = get_4_adjacency_graph({3, 3});
+        array_1d<int> edge_weights{1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1};
+
+        auto aligner = make_hierarchy_aligner_from_graph_cut(g, edge_weights);
+
+        hg::tree t(array_1d<index_t>{9, 10, 10, 9, 11, 11, 9, 11, 11, 13, 12, 12, 13, 13});
+        array_1d<int> altitudes{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2};
+        auto sm = aligner.align_hierarchy(t, altitudes);
+        auto sm_k = graph_4_adjacency_2_khalimsky(g, {3, 3}, sm);
+
+        array_1d<int> sm_coarse{2, 0, 0, 1, 1, 2, 0, 0, 0, 0, 2, 0};
+        auto sm2 = aligner.align_hierarchy(g, sm_coarse);
+        auto sm2_k = graph_4_adjacency_2_khalimsky(g, {3, 3}, sm2);
+
+        array_2d<int> sm_k_ref = {{0, 2, 0, 1, 0},
+                                  {0, 2, 1, 1, 0},
+                                  {0, 2, 0, 0, 0},
+                                  {0, 2, 0, 0, 0},
+                                  {0, 2, 0, 0, 0}};
+
+        BOOST_CHECK(sm_k == sm_k_ref);
+        BOOST_CHECK(sm2_k == sm_k_ref);
     }
 
 BOOST_AUTO_TEST_SUITE_END();
