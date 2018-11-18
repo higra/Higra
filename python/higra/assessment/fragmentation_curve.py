9f302152ab7f140439b07a6ee9386157630d36a9
Benjamin Perret <benjamin.perret@esiee.fr>
2018-11-18 13:45:37 +0100

    Add helper function python for assess fragmentation optimal cut

--- python/higra/assessment/fragmentation_curve.py
+++ python/higra/assessment/fragmentation_curve.py
@@ -34,3 +34,23 @@ def make_assesser_fragmentation_optimal_cut(tree,
     else:
         return hg.AssesserFragmentationOptimalCut(tree, ground_truth, measure, max_regions=max_regions,
                                                   vertex_map=vertex_map)
+
+
+@hg.argument_helper(hg.CptHierarchy, ("leaf_graph", hg.CptRegionAdjacencyGraph))
+def assess_fragmentation_optimal_cut(tree,
+                                     ground_truth,
+                                     measure,
+                                     max_regions=200,
+                                     vertex_map=None):
+    """
+    Compute the fragmentation curve of the optimal cuts in a hierarchy w.r.t. a given measure.
+
+    :param tree: input hierarchy
+    :param ground_truth: labelisation of base graph vertices (see parameter vertex_map if base_graph is a rag)
+    :param measure: evaluation measure to use (see enumeration OptimalCutMeasure)
+    :param max_regions: maximum number of regions in the cuts
+    :param vertex_map: vertex mapping if the hierarchy is build on super-vertices
+    :return: an object of type FragmentationCurve
+    """
+    assesser = make_assesser_fragmentation_optimal_cut(tree, ground_truth, measure, max_regions, vertex_map)
+    return assesser.fragmentation_curve()
