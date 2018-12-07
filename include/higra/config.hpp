0130b916dda92a2f45280b96477875a74b316f98
Benjamin Perret <benjamin.perret@esiee.fr>
2018-12-07 09:32:55 +0100

    Add versionning

---
+++ include/higra/config.hpp
@@ -0,0 +1,14 @@
+/***************************************************************************
+* Copyright ESIEE Paris (2018)                                             *
+*                                                                          *
+* Contributor(s) : Benjamin Perret                                         *
+*                                                                          *
+* Distributed under the terms of the CECILL-B License.                     *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+#pragma once
+
+#define HIGRA_VERSION_MAJOR 0
+#define HIGRA_VERSION_MINOR 1
+#define HIGRA_VERSION_PATCH 0
