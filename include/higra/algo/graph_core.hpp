f17ca2d8e069bbe5b1aa11b967a89df4d4329a7e
Benjamin Perret <benjamin.perret@esiee.fr>
2019-03-17 05:39:09 -0700

    Minimum spanning tree (cpp)

--- include/higra/algo/graph_core.hpp
+++ include/higra/algo/graph_core.hpp
@@ -12,6 +12,8 @@
 
 #include "../graph.hpp"
 #include "../algo/graph_weights.hpp"
+#include "higra/structure/unionfind.hpp"
+#include "xtensor/xview.hpp"
 
 namespace hg {
 
@@ -27,12 +29,12 @@ namespace hg {
      * @param edge_weights
      * @return
      */
-    template< typename graph_t,
+    template<typename graph_t,
             typename T>
-    auto graph_cut_2_labelisation(const graph_t & graph,
-                                  const xt::xexpression<T> & xedge_weights){
+    auto graph_cut_2_labelisation(const graph_t &graph,
+                                  const xt::xexpression<T> &xedge_weights) {
         HG_TRACE();
-        auto & edge_weights = xedge_weights.derived_cast();
+        auto &edge_weights = xedge_weights.derived_cast();
         hg_assert_edge_weights(graph, edge_weights);
         hg_assert_1d_array(edge_weights);
 
@@ -41,18 +43,18 @@ namespace hg {
         labels.fill(invalid_index);
 
         index_t current_label = 0;
-        for(auto v: vertex_iterator(graph)){
-            if(labels(v) == invalid_index){
+        for (auto v: vertex_iterator(graph)) {
+            if (labels(v) == invalid_index) {
                 current_label++;
                 labels(v) = current_label;
                 stack.push(v);
-                while(!stack.empty()){
+                while (!stack.empty()) {
                     auto cv = stack.top();
                     stack.pop();
-                    for(auto e: out_edge_iterator(cv, graph)){
-                        if(edge_weights(e) == 0){
+                    for (auto e: out_edge_iterator(cv, graph)) {
+                        if (edge_weights(e) == 0) {
                             auto n = target(e, graph);
-                            if(labels(n) == invalid_index){
+                            if (labels(n) == invalid_index) {
                                 labels(n) = current_label;
                                 stack.push(n);
                             }
@@ -77,17 +79,99 @@ namespace hg {
      * @param xvertex_labels
      * @return
      */
-    template< typename graph_t,
+    template<typename graph_t,
             typename T>
-    auto labelisation_2_graph_cut(const graph_t & graph,
-                                  const xt::xexpression<T> & xvertex_labels){
+    auto labelisation_2_graph_cut(const graph_t &graph,
+                                  const xt::xexpression<T> &xvertex_labels) {
         HG_TRACE();
-        auto & vertex_labels = xvertex_labels.derived_cast();
+        auto &vertex_labels = xvertex_labels.derived_cast();
         hg_assert_vertex_weights(graph, vertex_labels);
         hg_assert_1d_array(vertex_labels);
 
         return weight_graph<char>(graph, vertex_labels, weight_functions::L0);
     };
 
+
+    /**
+     * A simple structure to hold the result of minimum_spanning_tree function.
+     *
+     * The structures holds 2 elements:
+     *
+     *  - the minimum spanning tree (mst)
+     *  - a map (mst_edge_map) that indicates for each edge of the mst, the corresponding edge index in the original graph
+     *
+     * @tparam mst_t
+     */
+    template<typename mst_t>
+    struct minimum_spanning_tree_result {
+        mst_t mst;
+        array_1d<index_t> mst_edge_map;
+    };
+
+    /**
+     * Computes a minimum spanning tree of the given edge weighted graph using Kruskal's algorithm.
+     *
+     * The returned structure contains two elements:
+     *
+     *  - the minimum spanning tree (mst)
+     *  - a map (mst_edge_map) that indicates for each edge of the mst, the corresponding edge index in the input graph
+     *
+     * If the input graph is not connected, the result is indeed a minimum spanning forest.
+     *
+     * @tparam graph_t Input graph type
+     * @tparam T Input edge weights type
+     * @param graph Input graph
+     * @param xedge_weights  Input edge weights
+     * @return a mst structure
+     */
+    template<typename graph_t,
+            typename T>
+    auto minimum_spanning_tree(const graph_t &graph,
+                               const xt::xexpression<T> &xedge_weights) {
+        HG_TRACE();
+        auto &edge_weights = xedge_weights.derived_cast();
+        hg_assert_edge_weights(graph, edge_weights);
+        hg_assert_1d_array(edge_weights);
+
+        array_1d<index_t> sorted_edges_indices = xt::arange(num_edges(graph));
+        std::stable_sort(sorted_edges_indices.begin(), sorted_edges_indices.end(),
+                         [&edge_weights](index_t i, index_t j) { return edge_weights[i] < edge_weights[j]; });
+
+        auto num_points = num_vertices(graph);
+
+        auto num_edge_mst_max = num_points - 1;
+        ugraph mst(num_points);
+        array_1d<index_t> mst_edge_map = xt::empty<index_t>({num_edge_mst_max});
+
+        union_find uf(num_points);
+
+        size_t num_edge_found = 0;
+        index_t i = 0;
+
+        while (num_edge_found < num_edge_mst_max && i < (index_t) sorted_edges_indices.size()) {
+            auto ei = sorted_edges_indices[i];
+            auto e = edge_from_index(ei, graph);
+            auto c1 = uf.find(source(e, graph));
+            auto c2 = uf.find(target(e, graph));
+            if (c1 != c2) {
+                uf.link(c1, c2);
+                mst.add_edge(e);
+                mst_edge_map(num_edge_found) = ei;
+                num_edge_found++;
+            }
+            i++;
+        }
+
+        if (num_edge_found != num_edge_mst_max) {
+            return minimum_spanning_tree_result<ugraph>{
+                    std::move(mst),
+                    xt::view(mst_edge_map, xt::range(0, num_edge_found))};
+        } else{
+            return minimum_spanning_tree_result<ugraph>{
+                    std::move(mst),
+                    std::move(mst_edge_map)};
+        }
+
+    };
 
 }
--- test/cpp/algo/test_graph_core.cpp
+++ test/cpp/algo/test_graph_core.cpp
@@ -40,6 +40,59 @@ BOOST_AUTO_TEST_SUITE(test_cut);
             BOOST_CHECK(is_in_bijection(edge_weights, ref_edge_weights));
     }
 
+    BOOST_AUTO_TEST_CASE(test_minimum_spanning_tree) {
+            auto graph = get_4_adjacency_graph({2, 3});
+
+            xt::xarray<double> edge_weights{1, 0, 2, 1, 1, 1, 2};
+
+            auto res = minimum_spanning_tree(graph, edge_weights);
+            auto &mst = res.mst;
+            auto &mst_edge_map = res.mst_edge_map;
+
+            BOOST_CHECK(num_vertices(mst) == 6);
+            BOOST_CHECK(num_edges(mst) == 5);
+            std::vector<ugraph::edge_descriptor> ref = {{0, 3, 0},
+                                                        {0, 1, 1},
+                                                        {1, 4, 2},
+                                                        {2, 5, 3},
+                                                        {1, 2, 4}};
+            for (index_t i = 0; i < (index_t) ref.size(); i++) {
+                    auto e = edge_from_index(i, mst);
+                    BOOST_CHECK(e == ref[i]);
+            }
+            BOOST_CHECK(mst_edge_map == array_1d<int>({1, 0, 3, 4, 2}));
+    }
+
+    BOOST_AUTO_TEST_CASE(test_minimum_spanning_forest) {
+            ugraph graph(6);
+            add_edge(0, 1, graph);
+            add_edge(0, 2, graph);
+            add_edge(1, 2, graph);
+
+            add_edge(3, 4, graph);
+            add_edge(3, 5, graph);
+            add_edge(4, 5, graph);
+
+            xt::xarray<double> edge_weights{0, 1, 2, 3, 4, 5};
+
+            auto res = minimum_spanning_tree(graph, edge_weights);
+            auto &mst = res.mst;
+            auto &mst_edge_map = res.mst_edge_map;
+
+            BOOST_CHECK(num_vertices(mst) == 6);
+            BOOST_CHECK(num_edges(mst) == 4);
+            std::vector<ugraph::edge_descriptor> ref = {{0, 1, 0},
+                                                        {0, 2, 1},
+                                                        {3, 4, 2},
+                                                        {3, 5, 3}};
+            for (index_t i = 0; i < (index_t) ref.size(); i++) {
+                    auto e = edge_from_index(i, mst);
+                    BOOST_CHECK(e == ref[i]);
+            }
+
+            BOOST_CHECK(mst_edge_map == array_1d<int>({0, 1, 3, 4}));
+    }
+
 BOOST_AUTO_TEST_SUITE_END();
 
 
