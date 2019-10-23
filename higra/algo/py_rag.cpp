2f101d68de57979694355bd9fcbcfb1ce9132520
Benjamin Perret <benjamin.perret@esiee.fr>
2019-10-23 17:59:05 +0200

    fix forgotten accumulator during refactoring

--- higra/algo/py_rag.cpp
+++ higra/algo/py_rag.cpp
@@ -26,7 +26,8 @@ struct def_make_rag {
     template<typename value_t, typename C>
     static
     void def(C &c, const char *doc) {
-        c.def("_make_region_adjacency_graph_from_labelisation", [](const graph_t &graph, const pyarray<value_t> &input) {
+        c.def("_make_region_adjacency_graph_from_labelisation",
+              [](const graph_t &graph, const pyarray<value_t> &input) {
                   auto res = hg::make_region_adjacency_graph_from_labelisation(graph, input);
                   return py::make_tuple(std::move(res.rag), std::move(res.vertex_map), std::move(res.edge_map));
               },
@@ -65,50 +66,6 @@ struct def_rag_back_project_weights {
     }
 };
 
-struct def_rag_accumulate {
-    template<typename value_t, typename C>
-    static
-    void def(C &c, const char *doc) {
-        c.def("_rag_accumulate",
-              [](const pyarray<hg::index_t> &rag_map,
-                 const pyarray<value_t> &weights,
-                 hg::accumulators accumulator) {
-                  switch (accumulator) {
-                      case hg::accumulators::min:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_min());
-                          break;
-                      case hg::accumulators::max:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_max());
-                          break;
-                      case hg::accumulators::mean:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_mean());
-                          break;
-                      case hg::accumulators::counter:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_counter());
-                          break;
-                      case hg::accumulators::sum:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_sum());
-                          break;
-                      case hg::accumulators::prod:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_prod());
-                          break;
-                      case hg::accumulators::first:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_first());
-                          break;
-                      case hg::accumulators::last:
-                          return hg::rag_accumulate(rag_map, weights, hg::accumulator_last());
-                          break;
-                  }
-                  throw std::runtime_error("Unknown accumulator.");
-              },
-              doc,
-              py::arg("rag_map"),
-              py::arg("weights"),
-              py::arg("accumulator")
-        );
-    }
-};
-
 
 void py_init_rag(pybind11::module &m) {
     xt::import_numpy();
@@ -124,12 +81,6 @@ void py_init_rag(pybind11::module &m) {
     add_type_overloads<def_rag_back_project_weights, HG_TEMPLATE_NUMERIC_TYPES>
             (m,
              "Projects vertex or edge weights defined on a region adjacency graph back to the original graph space.");
-
-    add_type_overloads<def_rag_accumulate, HG_TEMPLATE_NUMERIC_TYPES>
-            (m,
-             "Accumulate vertex/edge weights of the original graph on vertex/edge of a region adjacency graph.");
-
-
 }
 
 
