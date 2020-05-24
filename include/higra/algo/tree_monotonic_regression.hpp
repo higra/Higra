298bfe4e9df7cbd26e68744fadde3e20b2ff5e8b
Benjamin Perret <benjamin.perret@esiee.fr>
2020-05-24 23:01:22 +0200

    add monotonic regression on tree documentation

--- include/higra/algo/tree_monotonic_regression.hpp
+++ include/higra/algo/tree_monotonic_regression.hpp
@@ -104,6 +104,46 @@ namespace hg {
         }
     }
 
+    /**
+     * Monotonic regression on the given tree altitudes. Computes new altitudes ``naltitudes`` that are *close* to the given
+     * :attr:`altitudes` and that are increasing for the given :attr:`tree`: i.e. for any nodes :math:`i, j` such that
+     * :math:`j` is an ancestor of :math:`i`, then :math:`naltitudes[i] \leq naltitudes[j]`.
+     *
+     * The definition of *close* depends of the value of :attr:`mode`:
+     *
+     * - If :attr:`mode` is equal to ``"min"`` then ``naltitudes`` is the largest increasing function
+     *   below :attr:`altitudes`.
+     * - If :attr:`mode` is equal to ``"max"`` then ``naltitudes`` is the smallest increasing function
+     *   above :attr:`altitudes`.
+     * - If :attr:`mode` is equal to ``"least_square"`` then ``naltitudes`` minizes the following minization problem:
+     *
+     * .. math::
+     *
+     *     naltitudes = \\arg \min_x \sum_i (weights[i] * (altitudes[i] - x[i])^2)
+     *
+     * such that ``naltitudes`` is increasing for :attr:`tree`.
+     *
+     * :Complexity:
+     *
+     * With :math:`n` the number of nodes in the :attr:`tree`:
+     *
+     * - For the modes ``"min"`` and ``"max"``, the runtime complexity is linear :math:`\mathcal{O}(n)`.
+     * - For the mode ``"least_square"``, the runtime complexity is linearithmic :math:`\mathcal{O}(n\log(n))` and the
+     *   space complexity is linear  :math:`\mathcal{O}(n)`. The algorithm used is described in:
+     *
+     *     P. Pardalos and G. Xue
+     *     `'Algorithms for a Class of Isotonic Regression Problems.' <https://link.springer.com/article/10.1007/PL00009258>`_
+     *     Algorithmica (1999) 23: 211. doi:10.1007/PL00009258
+     *
+     * @tparam tree_t
+     * @tparam T
+     * @tparam Tw
+     * @param tree input tree
+     * @param xaltitudes tree node altitudes
+     * @param xweights tree node weights
+     * @param mode "min", "max", or " least_square"
+     * @return
+     */
     template<typename tree_t, typename T, typename Tw>
     auto tree_monotonic_regression(const tree_t &tree, const xt::xexpression<T> &xaltitudes,
                                    const xt::xexpression<Tw> &xweights, const std::string &mode) {
