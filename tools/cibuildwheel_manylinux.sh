46d87946aa716168cc645e20c09a92e66c9f724f
SullyvanCOULON <sullyvancoulon@icloud.com>
2026-09-11 10:26:34 +0200

    build: migrate to oneTBB 2023.1
    - replace removed legacy task APIs with oneTBB primitives
    - migrate TBB discovery and linking to CMake's TBB::tbb target
    - migrate the existing parallel stable merge from legacy tbb::task to oneTBB parallel_invoke
    - update wheel build scripts and CI for oneTBB, including macOS ARM
    - add coverage for the parallel stable-sort path
    - add a sorting test for move-only values

--- tools/cibuildwheel_manylinux.sh
+++ tools/cibuildwheel_manylinux.sh
@@ -7,27 +7,21 @@ cur_dir=`pwd`
 
 export MFLAG="-m64"
 export CXXFLAGS=" ${MFLAG} -fabi-version=8 -Wno-error=changes-meaning"
-export TBB_URL="https://github.com/intel/tbb/archive/2019_U9.zip"
+export TBB_VERSION="2023.1.0"
+export TBB_URL="https://github.com/uxlfoundation/oneTBB/archive/refs/tags/v${TBB_VERSION}.tar.gz"
 
 curl -L "${TBB_URL}" -o archive.tgz
-unzip archive.tgz
-mv oneTBB* tbb
-cd tbb
-tbb_dir=`pwd`
+tar -xzf archive.tgz
+mv oneTBB-* tbb
 
-make -j tbb
+cmake -S tbb -B tbb/build \
+  -DTBB_TEST=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_INSTALL_PREFIX=/tbb
 
-cd build
-cd *release
-tbb_link=`pwd`
-
-mkdir -p /tbb/lib/
-mv ${tbb_link}/* /tbb/lib/
-mv ${tbb_dir}/include /tbb/
-ls /tbb/lib/*
-ls /tbb/include/*
-cd ${cur_dir}
-
+cmake --build tbb/build --parallel
+cmake --install tbb/build
 
 
 
